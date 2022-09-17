196bc8089b091c17fa615675784f3726aa2283d3
Etarious <ojameruayeetaoghene@gmail.com>
2022-09-17 03:17:22 +0100

    Completed the carousel, although more additional things might just be added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,13 +5,54 @@ let count = 0;
 
 let img = document.getElementById("img");
 
+let pointers = document.querySelectorAll(".point");
+// console.log(pointers[0].dataset.point);
+
+
+
+for (let i = 0; i < pointers.length; i++) {
+    pointers[i].addEventListener("click", () => {
+        // console.log(pointers[i].dataset.point);
+        for (let i = 0; i < pointers.length; i++) {
+            const poin = pointers[i];
+            poin.classList.remove("active");
+        }
+        pointers[i].classList.add("active");
+        count = pointers[i].dataset.point - 1;
+        img.src = "./public/img/" + images[count];
+    });
+    
+}
+
+
+
+
+function pointer() {
+    for (let i = 0; i < pointers.length; i++) {
+        const point = pointers[i];
+        if (img.dataset.index == point.dataset.point) {
+            // console.log(point);
+            point.classList.add("active");
+        }else{
+            point.classList.remove("active");
+        }
+    }
+}
+// pointer();
+
+
+
 function next() {
     if (count >= images.length - 1) {
         count = 0
         img.src = "./public/img/" + images[count];
+        img.dataset.index = count + 1;
+        pointer()
     }else{
         count++
         img.src = "./public/img/" + images[count];
+        img.dataset.index = count + 1;
+        pointer()
     }
 }
 
@@ -21,8 +62,18 @@ function previous() {
     if (count <= 0) {
         count = images.length - 1;
         img.src = "./public/img/" + images[count];
+        img.dataset.index = count + 1;
+        pointer();
     }else{
         count--
         img.src = "./public/img/" + images[count];
+        img.dataset.index = count + 1;
+        pointer();
     }
 }
+
+
+
+setInterval(() => {
+    next();
+}, 5000);
